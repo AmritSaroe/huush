4bbdf2698c7d8c4a3ac74b77504d354418e8c036
AmritSaroe <1.89528026e+08+AmritSaroe@users.noreply.github.com>
2026-08-24 19:12:35 +0000

    Harden targeted settings and motion fixes

--- client/src/lib/logger.js
+++ client/src/lib/logger.js
@@ -1,6 +1,6 @@
 import { Capacitor } from "@capacitor/core";
 
-export const APP_VERSION = "2.4.0";
+export const APP_VERSION = "2.4.1";
 const STORAGE_KEY = "huush_events";
 const LEGACY_STORAGE_KEY = "whitemint:logs";
 const ENABLED_KEY = "huush_logging_enabled";
