11e1a5e184068d5eb8f56c1ecc22fb301cb0e532
AmritSaroe <1.89528026e+08+AmritSaroe@users.noreply.github.com>
2026-08-27 04:31:12 +0000

    Remove selection toolbar black background

--- client/src/lib/logger.js
+++ client/src/lib/logger.js
@@ -1,6 +1,6 @@
 import { Capacitor } from "@capacitor/core";
 
-export const APP_VERSION = "2.6.14-selection-night-mode";
+export const APP_VERSION = "2.6.15-selection-bar";
 const STORAGE_KEY = "huush_events";
 const LEGACY_STORAGE_KEY = "whitemint:logs";
 const ENABLED_KEY = "huush_logging_enabled";
